bcbf0550d52c481a4636f1dfddc5273ff13cfd50
laguirre@uamv.edu.ni <laguirrev@uamv.edu.ni>
2026-06-01 23:16:53 -0600

    Enunciado

---
+++ Commit1.sql
@@ -0,0 +1,8 @@
+
+/*Crear una base de datos llamada UniversidadDB la cual maneja dos modulos:
+academico y seguridad
+
+Modulo academico: Carrera y Estudiante.
+
+Modelo Seguridad: Cargo y Usuario.
+*/
